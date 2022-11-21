3dee2e0e6192bba5dbc5488e2a9dd74fd28e6f16
dannyyyspam <danielaliabdulkarim@gmail.com>
2022-11-20 19:03:47 -0600

    style & implement data into tabs

--- src/app/components/game-tabs/game-tabs.component.ts
+++ src/app/components/game-tabs/game-tabs.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from 'src/app/models';
 
 @Component({
   selector: 'app-game-tabs',
   templateUrl: './game-tabs.component.html',
-  styleUrls: ['./game-tabs.component.scss']
+  styleUrls: ['./game-tabs.component.scss'],
 })
-export class GameTabsComponent {
+export class GameTabsComponent implements OnInit {
+  @Input()
+  game!: Game;
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
